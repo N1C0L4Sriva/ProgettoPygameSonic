9c3be3befce79037dc375d6d9389776336da5da4
saramosca2007 <sara2007.mosca@gmail.com>
2024-05-21 17:30:53 +0200

    inizio monete

--- monete.py
+++ monete.py
@@ -9,3 +9,4 @@ class Sonic(Oggettobase):
     def __init__(self, posizione: tuple = ..., velocità: tuple = ..., accelerazione: tuple = ..., grandezza: tuple = ..., percorsoimmagine: str = None, immagine: Surface = None, colore: tuple = ...):
         super().__init__(posizione, velocità, accelerazione, grandezza, percorsoimmagine, immagine, colore)
         
+
