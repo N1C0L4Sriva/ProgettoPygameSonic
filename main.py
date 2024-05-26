83c857d68037b7c1a06d3354690b420c7a4df255
N1C0L4Sriva <nico.panzerotto@gmail.com>
2024-05-26 15:40:48 +0200

    creato classe monete bnel main e creazione di queste casuale

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import pygame
 from sys import exit
 from random import randint
+# from monete import Monete
 
-from monete import Monete
+sonic_y=750
+sonic_x=300
+gravità=0 
+VelAvanza=4
 
+class Monete:
+    def __init__(self):
+        self.monete_pos_x=1100
+        self.monete_altezza=randint(300,650)
+        self.monete_pos_x=900
+        self.screen=screen        
+        self.moneta1=pygame.image.load('immaginiGioco/moneta1.png')
+
+    def aggiungimonete(self):
+        if keys[pygame.K_LEFT]:
+            self.monete_pos_x+=VelAvanza
+        if keys[pygame.K_RIGHT]:
+            self.monete_pos_x-=VelAvanza
+        screen.blit(self.moneta1,(self.monete_pos_x,self.monete_altezza))
+        
 def animation():
     global sonic_surface, sonic_index, sonic_index2
 
@@ -19,28 +38,19 @@ def animation():
             sonic_index=0
         sonic_surface=sonic_walk[int(sonic_index)]
 
-sonic_y=750
-sonic_x=300
-gravità=0 
-
-
-
-punteggio=0
-
-
 # def generamonete(posizionex, altezza): 
     # moneta= Monete((posizionex,altezza), (0,0),(0,0),(10,10))
     # screen.blit(moneta,monete_rect)
     # return moneta
 
-screen=pygame.display.set_mode((1200,800))
+screen=pygame.display.set_mode((1100,800))
 
 pygame.display.set_caption('sonic')
 
 sky_surface=pygame.image.load('immaginiGioco/background2.png').convert()
 
 ground_surface=pygame.image.load('immaginiGioco/pavimento1.png').convert()
-ground_rect=ground_surface.get_rect(topleft=(0,700))
+ground_rect=ground_surface.get_rect(topleft=(-100,700))
 
 ground2_surface=pygame.image.load('immaginiGioco/piattaforma.png').convert()
 ground2_x=600
@@ -58,10 +68,11 @@ sonic_index=0
 sonic_index2=0
 sonic_surface=sonic_walk[sonic_index]
 
-
+monete_tutte=[]
+monete_tutte.append(Monete())
+# monete=Monete(display)
 # moneta1=pygame.image.load('immaginiGioco/moneta1.png')
-# monete_tutte=[]
-# monete_tutte.append(Monete)
+
 
 Clock=pygame.time.Clock()
 
@@ -71,44 +82,65 @@ while True:
             pygame.quit()
             exit()
         
-        if event.type==pygame.KEYDOWN and sonic_rect.bottom>=650 or sonic_rect.colliderect(ground2_rect):
+        if event.type==pygame.KEYDOWN and sonic_rect.bottom>=650 or sonic_rect.colliderect(ground2_rect) :
             if event.key==pygame.K_UP:
                 gravità= -20
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,ground_rect)
     screen.blit(ground2_surface,ground2_rect)
-    
-    
-    #muovo sonic con la tastiera
-    
+ 
+    #muovo sonic con la tastiera 
     keys=pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
         # sonic_rect.right+=4
+        ground_rect.x-=4
+        if ground_rect.x<=-50:
+            ground_rect.x=0
         ground2_rect.x-=4
+
+
     if keys[pygame.K_LEFT]:
+        ground_rect.x+=4
+        if ground_rect.x>0:
+            ground_rect.x=-50
         ground2_rect.x+=4
+
+    if monete_tutte[-1].monete_pos_x<750:
+        monete_tutte.append(Monete())
+
+    for moneta in monete_tutte:
+        moneta.aggiungimonete()
 
     gravità+=1
     sonic_rect.y+=gravità
     if sonic_rect.colliderect(ground_rect)and sonic_rect.bottom>=700:
-
         sonic_rect.bottom=ground_rect.top +1
 
     if sonic_rect.colliderect(ground2_rect):
         sonic_rect.bottom=ground2_rect.top +1
 
-    
+    if sonic_rect.colliderect(ground2_rect) and sonic_rect.top==ground2_rect.bottom:
+        sonic_rect.top=ground2_rect.bottom
+        sonic_rect.y+=gravità
+
     animation()
-       
     screen.blit(sonic_surface,sonic_rect)
+
+    #creo monete
+    # monete_tutte.append(Monete)
+    # for monete in monete_tutte:
+    #     monete.draw(screen)
+
     # pygame.draw.rect(screen,(255,0,0), sonic_rect,1)
     # pygame.draw.rect(screen,(255,0,0), ground2_rect,1)
     # pygame.draw.rect(screen,(255,0,0), ground_rect, 1)
     # if keys[pygame.K_UP]:
         
-    
+    # monete.aggiungimonete()
 
+    # surf = pygame.transform.scale(display, (1200,800))
+    # screen.blit(surf, (0,0))
     pygame.display.update()
     Clock.tick(60)
 
--- monete.py
+++ monete.py
@@ -2,13 +2,25 @@ import pygame
 from pygame import Surface
 from classebase import Oggettobase
 from random import randint
-class Monete(Oggettobase):
-    def __init__(self, posizione: tuple = (randint(500,1200),), velocità: tuple = (0,0), accelerazione: tuple = (0,0), percorsoimmagine: str = None, immagine: Surface = None):
-        super().__init__(posizione, velocità, accelerazione, percorsoimmagine, moneta1=pygame.image.load('immaginiGioco/moneta1.png'))
-        self.presa=False
+# class Monete(Oggettobase):
+#     def __init__(self, posizione: tuple = (randint(500,1200),), velocità: tuple = (0,0), accelerazione: tuple = (0,0), percorsoimmagine: str = None, immagine: Surface = None):
+#         super().__init__(posizione, velocità, accelerazione, percorsoimmagine, moneta1=pygame.image.load('immaginiGioco/moneta1.png'))
+#         self.presa=False
 
-class Sonic(Oggettobase):
-    def __init__(self, posizione: tuple = ..., velocità: tuple = ..., accelerazione: tuple = ..., grandezza: tuple = ..., percorsoimmagine: str = None, immagine: Surface = None, colore: tuple = ...):
-        super().__init__(posizione, velocità, accelerazione, grandezza, percorsoimmagine, immagine, colore)
+# class Sonic(Oggettobase):
+#     def __init__(self, posizione: tuple = ..., velocità: tuple = ..., accelerazione: tuple = ..., grandezza: tuple = ..., percorsoimmagine: str = None, immagine: Surface = None, colore: tuple = ...):
+#         super().__init__(posizione, velocità, accelerazione, grandezza, percorsoimmagine, immagine, colore)
         
+class Monete:
+    def __init__(self):
+        self.screen=screen
+        self.altezza=randint(300,800)
+        self.pos_x=900
+        self.moneta1=pygame.image.load('immaginiGioco/moneta1.png')
+
+    def aggiungimonete(self):
+        screen.blit(self.moneta1,(self.pos_x,self.altezza))
+            
+        
+
 
