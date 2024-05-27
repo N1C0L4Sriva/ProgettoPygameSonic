3183528e955f71a5f8d291a21af9102e270122aa
N1C0L4Sriva <nico.panzerotto@gmail.com>
2024-05-27 16:40:49 +0200

    classe piattaforma

--- main.py
+++ main.py
@@ -6,25 +6,49 @@ from random import randint
 sonic_y=750
 sonic_x=300
 gravità=0 
-VelAvanza=4
-# class piattaforme:
-#     def __init__(self):
-#         self.screen=screen
-#         self.piattaforma1_x=600
-#         self.piattaforma1_y=550
-#         self.piattaforma1=pygame.image.load('immaginiGioco/piattaforma.png').convert()
+VelAvanza=7
 
-#     def draw_piattaforme(self):
-#         if keys[pygame.K_LEFT]:
-#             self.piattaforma1_x+=VelAvanza
-#         if keys[pygame.K_RIGHT]:
-#             self.piattaforma1_x-=VelAvanza
-#         self.piattaforma1_rect=self.piattaforma1.get_rect(topleft=(self.piattaforma1_x,self.piattaforma1_y))
-#         screen.blit(self.piattaforma1,self.piattaforma1_rect)
+class piattaforme:
+    def __init__(self):
+        self.screen=screen
+        self.piattaforma1_x=1100
+        self.piattaforma1_y=550
+        self.piattaforma1=pygame.image.load('immaginiGioco/piattaforma.png').convert()
+
+    def draw_piattaforme(self):
+        global gravità
+        for event in pygame.event.get():
+            if event.type==pygame.KEYDOWN and sonic_rect.colliderect(self.piattaforma1_rect):
+                if event.key==pygame.K_UP:
+                    gravità= -20  
+        if keys[pygame.K_LEFT]:
+            self.piattaforma1_x+=VelAvanza
+        if keys[pygame.K_RIGHT]:
+            self.piattaforma1_x-=VelAvanza
+        self.piattaforma1_rect=self.piattaforma1.get_rect(topleft=(self.piattaforma1_x,self.piattaforma1_y))
+        screen.blit(self.piattaforma1,self.piattaforma1_rect)
+    
+    def animazione_piattaforma(self):
+        global sonic_surface, sonic_index2, sonic_jump
+        if not sonic_rect.colliderect(self.piattaforma1_rect):
+            sonic_index2+=0.1
+            sonic_surface=sonic_jump
+        if sonic_index2>=len(sonic_jump):
+            sonic_index2=0
+        sonic_surface=sonic_jump[int(sonic_index2)]
+    
+    def mov_piattaforme(self):
+        if sonic_rect.colliderect(self.piattaforma1_rect):
+            sonic_rect.bottom=self.piattaforma1_rect.top +1
+
+        if sonic_rect.colliderect(self.piattaforma1_rect) and sonic_rect.top==self.piattaforma1_rect.bottom:
+            sonic_rect.top=self.piattaforma1_rect.bottom
+            sonic_rect.y+=gravità
+
    
 class Monete:
     def __init__(self):
-        self.monete_pos_x=1100
+        self.monete_pos_x=1300
         self.monete_altezza=randint(300,650)
         self.monete_pos_x=1200
         self.screen=screen        
@@ -56,7 +80,7 @@ class Monete:
 def animation():
     global sonic_surface, sonic_index, sonic_index2
 
-    if not sonic_rect.colliderect(ground2_rect) and not sonic_rect.colliderect(ground_rect):  #and sonic_rect.bottom<650:
+    if not sonic_rect.colliderect(ground_rect):  #and sonic_rect.bottom<650:
         sonic_index2+=0.1
         sonic_surface=sonic_jump
         if sonic_index2>=len(sonic_jump):
@@ -82,9 +106,9 @@ sky_surface=pygame.image.load('immaginiGioco/background2.png').convert()
 ground_surface=pygame.image.load('immaginiGioco/pavimento1.png').convert()
 ground_rect=ground_surface.get_rect(topleft=(-100,700))
 
-ground2_surface=pygame.image.load('immaginiGioco/piattaforma.png').convert()
-ground2_x=600
-ground2_rect=ground2_surface.get_rect(topleft=(ground2_x,550))
+# ground2_surface=pygame.image.load('immaginiGioco/piattaforma.png').convert()
+# ground2_x=600
+# ground2_rect=ground2_surface.get_rect(topleft=(ground2_x,550))
 
 
 sonic=pygame.image.load('immaginiGioco/sonic.png').convert_alpha()
@@ -98,8 +122,8 @@ sonic_index=0
 sonic_index2=0
 sonic_surface=sonic_walk[sonic_index]
 
-# piattaforme_tutte=[]
-# piattaforme_tutte.append(piattaforme())
+piattaforme_tutte=[]
+piattaforme_tutte.append(piattaforme())
 
 monete_tutte=[]
 monete_tutte.append(Monete())
@@ -115,13 +139,18 @@ while True:
             pygame.quit()
             exit()
         
-        if event.type==pygame.KEYDOWN and sonic_rect.bottom>=650 or sonic_rect.colliderect(ground2_rect) :
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+        
+        if event.type==pygame.KEYDOWN and sonic_rect.bottom>=650:
             if event.key==pygame.K_UP:
                 gravità= -20
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,ground_rect)
-    screen.blit(ground2_surface,ground2_rect)
+    # screen.blit(ground2_surface,ground2_rect)
  
     #muovo sonic con la tastiera 
     keys=pygame.key.get_pressed()
@@ -130,16 +159,21 @@ while True:
         ground_rect.x-=4
         if ground_rect.x<=-50:
             ground_rect.x=0
-        ground2_rect.x-=4
+        # ground2_rect.x-=4
 
     if keys[pygame.K_LEFT]:
         ground_rect.x+=4
         if ground_rect.x>0:
             ground_rect.x=-50
-        ground2_rect.x+=4
+        # ground2_rect.x+=4
 
-    # for platform in piattaforme_tutte:
-    #     platform.draw_piattaforme()
+    if piattaforme_tutte[-1].piattaforma1_x<350:
+        piattaforme_tutte.append(piattaforme())
+
+    for platform in piattaforme_tutte:
+        platform.draw_piattaforme()
+        platform.mov_piattaforme()
+        platform.animazione_piattaforma()
 
     if monete_tutte[-1].monete_pos_x<750:
         monete_tutte.append(Monete())
@@ -153,12 +187,12 @@ while True:
     if sonic_rect.colliderect(ground_rect)and sonic_rect.bottom>=700:
         sonic_rect.bottom=ground_rect.top +1
 
-    if sonic_rect.colliderect(ground2_rect):
-        sonic_rect.bottom=ground2_rect.top +1
+    # if sonic_rect.colliderect(piattaforme.piattaforma1_rect):
+    #     sonic_rect.bottom=p.top +1
 
-    if sonic_rect.colliderect(ground2_rect) and sonic_rect.top==ground2_rect.bottom:
-        sonic_rect.top=ground2_rect.bottom
-        sonic_rect.y+=gravità
+    # if sonic_rect.colliderect(p) and sonic_rect.top==p.bottom:
+    #     sonic_rect.top=ground2_rect.bottom
+    #     sonic_rect.y+=gravità
 
     animation()
     screen.blit(sonic_surface,sonic_rect)
