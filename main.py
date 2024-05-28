25911b47f91fee9859b7e00663307bf777c1f6dd
N1C0L4Sriva <nico.panzerotto@gmail.com>
2024-05-28 17:31:20 +0200

    cercato di aggiustare le piattaforme e aumentato la dimensione dei mostri

--- main.py
+++ main.py
@@ -6,47 +6,6 @@ from random import randint
 screen=pygame.display.set_mode((1100,800))
 
 pygame.display.set_caption('sonic')
-BLUE = (0, 0, 255)
-WHITE = (255, 255, 255)
-
-# Creazione della finestra
-pygame.display.set_caption('Sonic Main Menu')
-
-# Font per il testo
-font = pygame.font.SysFont("sonic font", 80)
-
-def draw_text(text, font, color, surface, x, y):
-    text_obj = font.render(text, True, color)
-    text_rect = text_obj.get_rect(center=(x, y))
-    surface.blit(text_obj, text_rect)
-
-def main_menu():
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_button.collidepoint(event.pos):
-                    print("Start button clicked!")
-                    # Qui potresti inserire la chiamata per avviare il gioco
-                    # start_game()
-
-        screen.fill(BLUE)
-
-        # Disegna il testo "START"
-        draw_text('START ', font, WHITE, screen, 1100 // 2, 800 // 2)
-
-        # Crea un rettangolo attorno al testo "START"
-        start_button = pygame.Rect((11000 // 2 - 100, 800 // 2 - 50), (200, 100))
-        pygame.draw.rect(screen, WHITE, start_button, 2)
-
-        pygame.display.flip()
-
-if __name__ == "__main__":
-    main_menu()
-
-
 
 sonic_y=750
 sonic_x=300
@@ -54,6 +13,7 @@ gravità=0
 VelAvanza=10
 
 class mob:
+    global keys ,gravità
     def __init__(self):
         self.screen=screen
         self.mob_x=randint(1300,1500)
@@ -87,10 +47,10 @@ class mob:
 class piattaforme:
     def __init__(self):
         self.screen=screen
-        self.piattaforma1_x=1100
-        self.piattaforma1_y=550
+        self.piattaforma1_x=randint(1100,1400)
+        self.piattaforma1_y=randint(400,600)
         self.piattaforma1=pygame.image.load('immaginiGioco/piattaforma.png').convert()
-        self.piattaforma1_rect=self.piattaforma1.get_rect(topleft=(self.piattaforma1_x,self.piattaforma1_y))
+        self.piattaforma1_rect=self.piattaforma1.get_rect(topleft=(self.piattaforma1_x,self.piattaforma1_y))       
 
     def draw_piattaforme(self):
         self.piattaforma1_rect=self.piattaforma1.get_rect(topleft=(self.piattaforma1_x,self.piattaforma1_y))
@@ -98,24 +58,28 @@ class piattaforme:
   
     def mov_piattaforme(self):
         global gravità
-
-        if keys[pygame.K_UP] and (sonic_rect.colliderect(ground_rect) or sonic_rect.colliderect(self.piattaforma1_rect)):
-            gravità= -20  
-
         if keys[pygame.K_LEFT]:
             self.piattaforma1_x+=VelAvanza
 
         if keys[pygame.K_RIGHT]:
             self.piattaforma1_x-=VelAvanza
 
+        if keys[pygame.K_UP] and (sonic_rect.colliderect(ground_rect)):
+            gravità=-20
+        
+        if keys[pygame.K_UP] and sonic_rect.colliderect(self.piattaforma1_rect):
+            gravità=-20
+
         if sonic_rect.colliderect(self.piattaforma1_rect):
-            sonic_rect.bottom=550
-
-        if sonic_rect.colliderect(self.piattaforma1_rect) and sonic_rect.top==self.piattaforma1_rect.bottom:
-            sonic_rect.top=self.piattaforma1_rect.bottom
-            sonic_rect.y+=gravità
-
-   
+            if gravità>0:
+                sonic_rect.bottom=self.piattaforma1_rect.top
+            if gravità<=0:
+                gravità=10
+                sonic_rect.top=self.piattaforma1_rect.bottom
+           
+        
+            
+      
 class Monete:
     def __init__(self):
         self.monete_pos_x=1300
@@ -167,12 +131,12 @@ sonic=pygame.image.load('immaginiGioco/sonic.png').convert_alpha()
 sonic_rect=sonic.get_rect(bottomleft=(sonic_x,sonic_y))
 sonic2=pygame.image.load('immaginiGioco/sonic2.png').convert_alpha()
 sonic_walk=[sonic,sonic2]
-sonic_jump1=pygame.image.load('immaginiGioco/sonic_jump_1.png').convert_alpha()
-sonic_jump2=pygame.image.load('immaginiGioco/sonic_jump_2.png').convert_alpha()
-sonic_jump=[sonic_jump1,sonic_jump2]
 sonic_index=0
 sonic_index2=0
 sonic_surface=sonic_walk[sonic_index]
+
+larghezza_sonic2=sonic_surface.get_width()
+altezza_sonic2=sonic_surface.get_height()
 
 piattaforme_tutte=[]
 piattaforme_tutte.append(piattaforme())
@@ -186,63 +150,107 @@ mob_tutti.append(mob())
 
 Clock=pygame.time.Clock()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_ESCAPE:
+def gioco():
+    global gravità, piattaforma1_rect, keys
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+        
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_ESCAPE:
+                    pygame.quit()
+                    exit()
 
-    screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface,ground_rect)
+        screen.blit(sky_surface,(0,0))
+        screen.blit(ground_surface,ground_rect)
 
-    animation()
-    screen.blit(sonic_surface,sonic_rect)
+        animation()
+        screen.blit(sonic_surface,sonic_rect)
 
-    gravità+=1
-    sonic_rect.y+=gravità
+        gravità+=1
+        sonic_rect.y+=gravità
 
-    #muovo sonic con la tastiera 
-    keys=pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT]:
-        ground_rect.x-=4
-        if ground_rect.x<=-50:
-            ground_rect.x=0
+        #muovo sonic con la tastiera 
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            ground_rect.x-=4
+            if ground_rect.x<=-50:
+                ground_rect.x=0
 
-    if keys[pygame.K_LEFT]:
-        ground_rect.x+=4
-        if ground_rect.x>0:
-            ground_rect.x=-50
+        if keys[pygame.K_LEFT]:
+            ground_rect.x+=4
+            if ground_rect.x>0:
+                ground_rect.x=-50
 
-    if piattaforme_tutte[-1].piattaforma1_x<350:
-        piattaforme_tutte.append(piattaforme())
+        if piattaforme_tutte[-1].piattaforma1_x<850:
+            piattaforme_tutte.append(piattaforme())
 
-    if sonic_rect.colliderect(ground_rect)and sonic_rect.bottom>=700:
-        sonic_rect.bottom=ground_rect.top +1
+        if sonic_rect.colliderect(ground_rect)and sonic_rect.bottom>=700:
+            sonic_rect.bottom=ground_rect.top +1
     
-    for platform in piattaforme_tutte:
-        platform.mov_piattaforme()
-        platform.draw_piattaforme()
+        for platform in piattaforme_tutte:
+            platform.mov_piattaforme()
+            platform.draw_piattaforme()
     
-    if monete_tutte[-1].monete_pos_x<750:
-        monete_tutte.append(Monete())
+        if monete_tutte[-1].monete_pos_x<750:
+            monete_tutte.append(Monete())
     
-    for moneta in monete_tutte:
-        moneta.animazione_monete()
-        moneta.aggiungimonete()
+        for moneta in monete_tutte:
+            moneta.animazione_monete()
+            moneta.aggiungimonete()
     
-    if mob_tutti[-1].mob_rect.x<650:
-        mob_tutti.append(mob())
+        if mob_tutti[-1].mob_rect.x<650:
+            mob_tutti.append(mob())
 
-    for mostro in mob_tutti:
-        mostro.movimento_mob()
-        mostro.draw_mob()
+        for mostro in mob_tutti:
+            mostro.movimento_mob()
+            mostro.draw_mob()
 
-    pygame.display.update()
-    Clock.tick(60+pygame.time.get_ticks()//2000)
+        pygame.display.update()
+        Clock.tick(60+pygame.time.get_ticks()//2000)
 
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
 
+# Creazione della finestra
+pygame.display.set_caption('Sonic Main Menu')
+
+# Font per il testo
+font = pygame.font.SysFont("sonic font", 80)
+
+def draw_text(text, font, color, surface, x, y):
+    text_obj = font.render(text, True, color)
+    text_rect = text_obj.get_rect(center=(x, y))
+    surface.blit(text_obj, text_rect)
+
+def main_menu():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if sonic_logo_rect.collidepoint(event.pos):
+                    print("Start button clicked!")
+                    # Qui potresti inserire la chiamata per avviare il gioco
+                    gioco()
+
+        screen.fill(BLUE)
+
+        # Disegna il testo "START"
+        sonic_logo=pygame.image.load('immaginiGioco/sonicLogo.png')
+        draw_text('START ', font, WHITE, screen, 1100 // 2, 800 // 2)
+
+        # Crea un rettangolo attorno al testo "START"
+        sonic_logo=pygame.image.load('immaginiGioco/sonicLogo.png')
+        sonic_logo_rect=sonic_logo.get_rect(bottomleft=(350,300))
+        start_button = pygame.Rect((11000 // 2 - 100, 800 // 2 - 50), (200, 100))
+        pygame.draw.rect(screen, WHITE, start_button, 2)
+        screen.blit(sonic_logo,sonic_logo_rect)
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main_menu()
